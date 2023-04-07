605c6e96c7df5c25226b5ee70e9e6ffeda139d4a
alesso-chiavarino <alessochiavarino@gmail.com>
2023-04-07 16:49:28 -0300

    feat(package.json): añadir nextjs-progressbar como dependencia feat(_app.js): añadir NextNProgress como componente para mostrar una barra de progreso en la carga de páginas

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css'
 import BotProvider from '@/context/botContext'
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }) {
   return (
     <BotProvider>
+      <NextNProgress color='#895DF5' />
       <Component {...pageProps} />
     </BotProvider>
   )
